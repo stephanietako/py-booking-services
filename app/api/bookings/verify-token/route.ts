96680eb3a42e2589d7f1d5481d7b66a93127cf25
stephanietako <gabeshine@live.fr>
2025-04-24 19:49:43 +0200

    v1: fixed errors & optimizations part8

--- app/api/bookings/verify-token/route.ts
+++ app/api/bookings/verify-token/route.ts
@@ -25,6 +25,10 @@ export async function POST(req: NextRequest) {
 
     const decoded = jwt.verify(token, secretKey as string) as CustomJwtPayload;
 
+    if (!decoded || !decoded.bookingId || !decoded.userId) {
+      return NextResponse.json({ error: "Token invalide" }, { status: 400 });
+    }
+
     const booking = await prisma.booking.findUnique({
       where: { id: decoded.bookingId },
       include: { user: true },
@@ -59,51 +63,3 @@ export async function POST(req: NextRequest) {
     );
   }
 }
-
-// import { NextRequest, NextResponse } from "next/server";
-// import jwt, { JwtPayload } from "jsonwebtoken";
-// import { prisma } from "@/lib/prisma";
-
-// const secretKey = process.env.JWT_SECRET;
-
-// if (!secretKey) {
-//   throw new Error("JWT_SECRET is missing in the environment variables");
-// }
-
-// export async function POST(req: NextRequest) {
-//   try {
-//     const { token } = await req.json();
-
-//     if (!token) {
-//       return NextResponse.json({ error: "Token manquant" }, { status: 400 });
-//     }
-
-//     const decoded = jwt.verify(token, secretKey as string) as JwtPayload & {
-//       bookingId: string;
-//       userId: string;
-//     };
-
-//     const booking = await prisma.booking.findUnique({
-//       where: { id: decoded.bookingId },
-//       include: { user: true },
-//     });
-
-//     console.log("Réservation vérifiée");
-
-//     if (!booking || booking.user.clerkUserId !== decoded.userId) {
-//       console.log("Accès non autorisé.");
-//       return NextResponse.json(
-//         { error: "Accès non autorisé" },
-//         { status: 403 }
-//       );
-//     }
-
-//     return NextResponse.json({ decoded }, { status: 200 });
-//   } catch (error) {
-//     console.error("Erreur lors de la vérification :", error);
-//     return NextResponse.json(
-//       { error: "Erreur lors de la vérification" },
-//       { status: 500 }
-//     );
-//   }
-// }
