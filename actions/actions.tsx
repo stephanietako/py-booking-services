02dee2c42763deb8094d33820b3e7e2f814195e7
stephanietako <gabeshine@live.fr>
2025-04-24 19:41:05 +0200

    v1: fixed errors & optimizations part7

--- actions/actions.tsx
+++ actions/actions.tsx
@@ -81,20 +81,8 @@ export async function addUserToDatabase(
 }
 
 // Récupérer les services associés à un utilisateur
-// export async function getServicesByUser(clerkUserId: string) {
-//   try {
-//     const user = await prisma.user.findUnique({
-//       where: { clerkUserId },
-//       include: { bookings: { include: { service: true } } },
-//     });
-//     if (!user) throw new Error("Utilisateur non trouvé");
-//     return user.bookings.map((booking) => booking.service);
-//   } catch (error) {
-//     console.error("Erreur lors de la récupération des services:", error);
-//     throw error;
-//   }
-// }
-// Typage explicite pour les paramètres
+// Récupérer les services associés à un utilisateur
+// Récupérer les services associés à un utilisateur
 export async function getServicesByUser(clerkUserId: string) {
   try {
     const user = await prisma.user.findUnique({
@@ -104,9 +92,12 @@ export async function getServicesByUser(clerkUserId: string) {
 
     if (!user) throw new Error("Utilisateur non trouvé");
 
-    // Typage explicite pour les éléments de `bookings`
+    // Vérification du type des objets dans user.bookings
     return user.bookings
-      .filter((b) => b.service !== null && b.service !== undefined) // Vérifier si `service` existe
+      .filter(
+        (b: { service: Service | null }) =>
+          b.service !== null && b.service !== undefined
+      ) // Vérifier si `service` existe
       .map((b) => b.service!); // Utilisation de `!` pour indiquer que `service` est défini après la vérification
   } catch (error) {
     console.error("Erreur lors de la récupération des services:", error);
@@ -114,44 +105,6 @@ export async function getServicesByUser(clerkUserId: string) {
   }
 }
 
-// Récupérer un service par son ID
-// export async function getServiceById(serviceId: string) {
-//   try {
-//     const service = await prisma.service.findUnique({
-//       where: { id: serviceId },
-//     });
-
-//     if (!service) {
-//       throw new Error("Service non trouvé");
-//     }
-
-//     return service; // Retourne le service avec la description
-//   } catch (error) {
-//     console.error("❌ Erreur lors de la récupération du service.");
-//     throw error;
-//   }
-// }
-
-// Récupérer la liste des options associées à un service
-// export async function getOptionsByServiceId(serviceId: string) {
-//   try {
-//     const service = await prisma.service.findUnique({
-//       where: { id: serviceId },
-//       include: {
-//         options: true,
-//       },
-//     });
-
-//     if (!service) {
-//       throw new Error("Service non trouvé");
-//     }
-
-//     return service.options;
-//   } catch (error) {
-//     console.error("❌ Erreur lors de la récupération des options.");
-//     throw error;
-//   }
-// }
 export async function getOptionsByServiceId(serviceId: string) {
   try {
     const service = await prisma.service.findUnique({
@@ -173,29 +126,6 @@ export async function getOptionsByServiceId(serviceId: string) {
   }
 }
 
-// Ajouter une option à un service
-// export async function addOptionToService(
-//   serviceId: string,
-//   amount: number,
-//   description: string
-// ) {
-//   try {
-//     const option = await prisma.option.create({
-//       data: {
-//         amount,
-//         description,
-//         serviceId,
-//       },
-//     });
-
-//     console.log("✅ Option ajoutée avec succès");
-
-//     return option;
-//   } catch (error) {
-//     console.error("Erreur lors de l'ajout de la option:", error);
-//     throw error;
-//   }
-// }
 export async function addOptionToService(
   serviceId: string,
   amount: number,
@@ -329,35 +259,6 @@ export async function getOptionsByEmailAndPeriod(
   }
 }
 
-// Récupérer les services associés à un utilisateur
-// export const getLastServices = async (clerkUserId: string) => {
-//   try {
-//     const services = await prisma.service.findMany({
-//       where: {
-//         bookings: {
-//           some: {
-//             userId: clerkUserId,
-//           },
-//         },
-//       },
-//       orderBy: {
-//         createdAt: "desc",
-//       },
-//       take: 3,
-//       include: {
-//         options: true,
-//       },
-//     });
-
-//     return services;
-//   } catch (error) {
-//     console.error(
-//       "Erreur lors de la récupération des derniers services",
-//       error
-//     );
-//     throw error;
-//   }
-// };
 export const getLastServices = async (
   clerkUserId: string
 ): Promise<Service[]> => {
@@ -436,44 +337,6 @@ export async function createService(
 
 //////////
 // Récupérer les services atteints par un utilisateur (exemple: services ayant plus de 5 options)
-// export async function getReachedServices(
-//   clerkUserId: string,
-//   optionThreshold: number = 5
-// ) {
-//   try {
-//     const user = await prisma.user.findUnique({
-//       where: { clerkUserId },
-//       include: {
-//         bookings: {
-//           include: {
-//             service: {
-//               include: {
-//                 options: true,
-//               },
-//             },
-//           },
-//         },
-//       },
-//     });
-
-//     if (!user) {
-//       throw new Error("Utilisateur non trouvé");
-//     }
-
-//     // Filtre les services dont le nombre d' options dépasse le seuil
-//     const reachedServices = user.bookings
-//       .map((booking) => booking.service)
-//       .filter((service) => service.options.length >= optionThreshold);
-
-//     return reachedServices;
-//   } catch (error) {
-//     console.error(
-//       "Erreur lors de la récupération des services atteints:",
-//       error
-//     );
-//     throw error;
-//   }
-// }
 export async function getReachedServices(
   clerkUserId: string,
   optionThreshold: number = 5
@@ -518,75 +381,6 @@ export async function getReachedServices(
 }
 
 // Calculer le montant total des optionss d'un utilisateur dans une période donnée
-// export async function getTotalOptionAmount(
-//   clerkUserId: string,
-//   period: string
-// ) {
-//   try {
-//     const now = new Date();
-//     let dateLimit;
-
-//     switch (period) {
-//       case "last30":
-//         dateLimit = new Date(now);
-//         dateLimit.setDate(now.getDate() - 30);
-//         break;
-//       case "last90":
-//         dateLimit = new Date(now);
-//         dateLimit.setDate(now.getDate() - 90);
-//         break;
-//       case "last7":
-//         dateLimit = new Date(now);
-//         dateLimit.setDate(now.getDate() - 7);
-//         break;
-//       case "last365":
-//         dateLimit = new Date(now);
-//         dateLimit.setFullYear(now.getFullYear() - 1);
-//         break;
-//       default:
-//         throw new Error("Période invalide");
-//     }
-
-//     const user = await prisma.user.findUnique({
-//       where: { clerkUserId },
-//       include: {
-//         bookings: {
-//           include: {
-//             service: {
-//               include: {
-//                 options: {
-//                   where: {
-//                     createdAt: {
-//                       gte: dateLimit,
-//                     },
-//                   },
-//                 },
-//               },
-//             },
-//           },
-//         },
-//       },
-//     });
-
-//     if (!user) {
-//       throw new Error("Utilisateur non trouvé");
-//     }
-
-//     // Calcule le montant total des options
-//     const totalAmount = user.bookings.reduce((total, booking) => {
-//       const serviceTotal = booking.service.options.reduce(
-//         (amountTotal, option) => amountTotal + option.amount,
-//         0
-//       );
-//       return total + serviceTotal;
-//     }, 0);
-
-//     return totalAmount;
-//   } catch (error) {
-//     console.error("Erreur lors du calcul du montant total des options:", error);
-//     throw error;
-//   }
-// }
 export async function getTotalOptionAmount(
   clerkUserId: string,
   period: string
@@ -661,68 +455,6 @@ export async function getTotalOptionAmount(
 }
 
 // Calculer le nombre total d'options d'un utilisateur dans une période donnée
-// export async function getTotalOptionCount(clerkUserId: string, period: string) {
-//   try {
-//     const now = new Date();
-//     let dateLimit;
-
-//     switch (period) {
-//       case "last30":
-//         dateLimit = new Date(now);
-//         dateLimit.setDate(now.getDate() - 30);
-//         break;
-//       case "last90":
-//         dateLimit = new Date(now);
-//         dateLimit.setDate(now.getDate() - 90);
-//         break;
-//       case "last7":
-//         dateLimit = new Date(now);
-//         dateLimit.setDate(now.getDate() - 7);
-//         break;
-//       case "last365":
-//         dateLimit = new Date(now);
-//         dateLimit.setFullYear(now.getFullYear() - 1);
-//         break;
-//       default:
-//         throw new Error("Période invalide");
-//     }
-
-//     const user = await prisma.user.findUnique({
-//       where: { clerkUserId },
-//       include: {
-//         bookings: {
-//           include: {
-//             service: {
-//               include: {
-//                 options: {
-//                   where: {
-//                     createdAt: {
-//                       gte: dateLimit,
-//                     },
-//                   },
-//                 },
-//               },
-//             },
-//           },
-//         },
-//       },
-//     });
-
-//     if (!user) {
-//       throw new Error("Utilisateur non trouvé");
-//     }
-
-//     // Calcule le nombre total d' options
-//     const totalCount = user.bookings.reduce((count, booking) => {
-//       return count + booking.service.options.length;
-//     }, 0);
-
-//     return totalCount;
-//   } catch (error) {
-//     console.error("Erreur lors du calcul du nombre total des options:", error);
-//     throw error;
-//   }
-// }
 export async function getTotalOptionCount(
   clerkUserId: string,
   period: string
