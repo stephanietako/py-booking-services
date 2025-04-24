56d65cf6bbe63c30da37134235bd52572d6f4960
stephanietako <gabeshine@live.fr>
2025-04-24 14:53:31 +0200

    v1: Update dependencies and adjust configurations & Balises Open Graph

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,61 @@
+// //import { getClosedDays, getDays } from "@/actions/openingActions"; // Les actions que tu utilises pour récupérer les données
+// import Wrapper from "./components/Wrapper/Wrapper";
+// import Hero from "./components/Hero/Hero";
+// import Location from "./components/Location/Location";
+
+// export const dynamic = "force-dynamic";
+
+// const Home = async () => {
+//   //const days = await getDays(); // Récupérer les jours d'ouverture
+//   //const closedDays = await getClosedDays(); // Récupérer les jours fermés
+
+//   return (
+//     <Wrapper>
+//       <main>
+//         <Hero />
+//         <Location />
+//       </main>
+//     </Wrapper>
+//   );
+// };
+
+// export default Home;
 //import { getClosedDays, getDays } from "@/actions/openingActions"; // Les actions que tu utilises pour récupérer les données
 import Wrapper from "./components/Wrapper/Wrapper";
 import Hero from "./components/Hero/Hero";
 import Location from "./components/Location/Location";
+import Head from "next/head";
 
 export const dynamic = "force-dynamic";
 
-const Home = async () => {
-  //const days = await getDays(); // Récupérer les jours d'ouverture
-  //const closedDays = await getClosedDays(); // Récupérer les jours fermés
-
+const Home = () => {
   return (
-    <Wrapper>
-      <main>
-        <Hero />
-        <Location />
-      </main>
-    </Wrapper>
+    <>
+      <Head>
+        {/* Balises Open Graph */}
+        <meta
+          property="og:title"
+          content="Location de bateau à Cavalaire-sur-Mer – Cap Camarat 12.5 WA avec skipper"
+        />
+        <meta
+          property="og:description"
+          content="Profitez d'une excursion privée en mer à bord d'un Cap Camarat 12.5 WA de 2021, avec skipper au départ de Cavalaire-sur-Mer. Bateau spacieux, récent et tout équipé pour un confort exceptionnel."
+        />
+        <meta
+          property="og:image"
+          content="https://yachting-day.com/assets/images/hero.webp"
+        />
+        <meta property="og:url" content="https://yachting-day.com" />
+        <meta property="og:type" content="website" />
+        <meta property="og:locale" content="fr_FR" />
+      </Head>
+      <Wrapper>
+        <main>
+          <Hero />
+          <Location />
+        </main>
+      </Wrapper>
+    </>
   );
 };
 
